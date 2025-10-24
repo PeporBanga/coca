734430911166f73fe48e66e4e1543b8c856645b9
Pepor_banga <romanoff.alexand2014@yandex.ru>
2025-10-24 15:33:14 +0500

    Created insight section and added swiper

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+useTestimonialsSlider();
