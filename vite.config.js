e1785b77f2233870d2b4802ec5004c3d667a5e5e
Pepor_banga <romanoff.alexand2014@yandex.ru>
2025-11-01 13:41:20 +0500

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: './src',
+  base: '/coca/',
   server: {
     port: 3000,
   },
+  publicDir:'./scr/assets',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
